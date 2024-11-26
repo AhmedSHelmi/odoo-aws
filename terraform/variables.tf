bcc381af1c91f19847ad80ad6ecb1f6e0941056c
Ahmed Helmi <26045423+AhmedSHelmi@users.noreply.github.com>
2024-11-26 03:54:20 +0200

    test: keys passed to command

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,15 @@ variable "instance_type" {
 variable "key_name" {
   description = "Name of the EC2 key pair"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
